76140e133b46c5b3709c4ab1da68ff484f4ff9b0
songkejun <skj@cheok.com>
2020-12-24 17:14:05 +0800

    首页

--- pages/message/index.js
+++ pages/message/index.js
@@ -1,7 +1,26 @@
 // pages/message/index.js
 Page({
-  data:{
-    datasource:[{},{},{},{}]
+  data: {
+    datasource: [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}],
+    index:0
   },
-
+  clickTab: function (e) {
+    wx.getSystemInfo({
+      success: (result) => {
+        console.log(result)
+      },
+    })
+    let index = e.currentTarget.dataset.index;
+    if(index == 0){
+      this.setData({
+        index:0,
+        toView:'item0'
+      })
+    } else {
+      this.setData({
+        index:1,
+        toView:'item1'
+      })
+    }
+  },
 })
